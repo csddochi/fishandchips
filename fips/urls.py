3b81ef00bff1d85ca6aeb8c7363b8187a757628a
csddochi <thfudwofud@naver.com>
2017-03-09 15:49:10 -0800

    fixed render to redirect for URL

--- fips/urls.py
+++ fips/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.introduction, name='introduction'),
-    url(r'^login/', views.sign_in, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^main/$', views.main, name='main'),
+    url(r'^login/', views.sign_in, name='login'),
+    url(r'^logout/$', views.log_out, {'next_page': '/'}, name='logout'),
+
 ]
--- fips/views.py
+++ fips/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import make_password
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def introduction(request):
@@ -13,29 +14,27 @@ def sign_in(request):
     password = request.POST.get('password', '')
     print(request.POST)
     user = authenticate(username=username, password=password)
-    print('result:', user)
     if user is not None:
         login(request, user)
-        return render(request, 'fips/main.html', {})
+        return HttpResponseRedirect('/main/')
     else: # create a user account
         return sign_up(request, username, password)
 
 def sign_up(request, username, password):
     email = request.POST.get('email')
     _referrer = request.POST.get('referrer')
-    print('email:', email, '/ referrer:', _referrer)
     if email is not None and _referrer is not None:
         _password = make_password(password)
         _user = User(username=username, email=email, password=_password)
         _user.referrer = _referrer
-        print('ref_code:', _user.ref_code)
         _user.save()
         return render(request, 'registration/sign-up.html', {})
     else:
         return render(request, 'registration/login.html', {})
 
-def logout(request):
-    return render(request, 'fips/introduction.html', {})
+def log_out(request):
+    logout(request)
+    return render(request, 'fips/login.html', {})
 
 @login_required
 def main(request):
